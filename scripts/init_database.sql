9f7d1177e0a492de3bbd9ee8d69ad7916493bcf4
dataseeker07 <haniyasarbudeen@gmail.com>
2026-04-13 15:31:52 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,24 @@
+/*   */
 
+use master;
+go
+
+--drop and recreating the "DataWarehouse" database 
+if exists 
+--creating database
+create database DataWarehouse;
+
+--switch the database
+use DataWarehouse;
+
+--creatng schema "bronze"
+create schema bronze;
+go
+
+--creatng schema "bronze"
+create schema silver;
+go
+
+--creatng schema "bronze" 
+create schema gold;
+go
